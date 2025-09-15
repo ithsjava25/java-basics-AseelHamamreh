3a9b006dd803635bc2dc2e09bd5bec47bbd7156e
AseelHamamreh <aseel.hamamreh@hotmail.com>
2025-09-16 01:01:49 +0200

    adding optimal window function

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -4,6 +4,7 @@ import com.example.api.ElpriserAPI;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -41,56 +42,124 @@ public class Main {
         }
 
 
+        // checking functions:
 
-        calculateMeanMaxMinPrice(todaysList);
+
+        findMeanPrice(todaysList);
+        findMinPrice(todaysList);
+        findMaxPrice(todaysList);
 
         sort(todaysList);
 
+        optimalWindow(todaysList, 8);
+
 
 
     }
 
 
 
-    //  Function for calculating and displaying the mean, max, and min price for the current 24-hour period:
+    //  Function for calculating and displaying the mean price:
 
-
-    public static void calculateMeanMaxMinPrice(List<ElpriserAPI.Elpris> pricesList){
+    public static void findMeanPrice(List<ElpriserAPI.Elpris> pricesList){
         double sum=0;
-        double cheapestPrice = pricesList.get(0).sekPerKWh();
-        double expensivePrice = pricesList.get(0).sekPerKWh();
-
-        int cheapestHour = pricesList.get(0).timeStart().getHour();
-        int expensiveHour = pricesList.get(0).timeStart().getHour();
-
         for (int i=0; i<pricesList.size(); i++){
             sum+=pricesList.get(i).sekPerKWh();
-            if(pricesList.get(i).sekPerKWh() > expensivePrice){
-                expensivePrice = pricesList.get(i).sekPerKWh();
-                expensiveHour = pricesList.get(i).timeStart().getHour();
-            }
-            if(pricesList.get(i).sekPerKWh() < cheapestPrice){
-                cheapestPrice = pricesList.get(i).sekPerKWh();
-                cheapestHour = pricesList.get(i).timeStart().getHour();
-            }
         }
-
         double mean = sum / pricesList.size();
         System.out.println("mean price = " + mean);
-        System.out.println("cheapest price hour: " + cheapestHour);
-        System.out.println("most expensive Price hour: " + expensiveHour);
 
     }
+
+    //  Function for calculating and displaying the minimum price:
+
+
+    public static void findMinPrice(List<ElpriserAPI.Elpris> pricesList){
+        ElpriserAPI.Elpris min = pricesList.get(0);
+
+        for (int i=0; i<pricesList.size(); i++) {
+            if(pricesList.get(i).sekPerKWh() < min.sekPerKWh()){
+                min = pricesList.get(i);
+            }
+        }
+        System.out.println("cheapest price is = " + min.sekPerKWh()  + " at " + min.timeStart().getHour() + " oclock.");
+
+    }
+
+
+    //  Function for calculating and displaying the maximum price:
+
+    public static void findMaxPrice(List<ElpriserAPI.Elpris> pricesList){
+        ElpriserAPI.Elpris max = pricesList.get(0);
+
+        for (int i=0; i<pricesList.size(); i++) {
+            if(pricesList.get(i).sekPerKWh() > max.sekPerKWh()){
+                max = pricesList.get(i);
+            }
+        }
+        System.out.println("most expensive price is = " + max.sekPerKWh()  + " at " + max.timeStart().getHour() + " oclock.");
+
+
+    }
+
+
 
     // function for displaying prices in descending order:
 
-
     public static void sort(List<ElpriserAPI.Elpris> pricesList){
 
-        pricesList.sort((a, b) -> Double.compare(b.sekPerKWh(), a.sekPerKWh()));
+        List<ElpriserAPI.Elpris> sortedCopy = new ArrayList<>(pricesList);
 
-        System.out.println("list after sorting: " + pricesList);
+        sortedCopy.sort((a, b) -> Double.compare(b.sekPerKWh(), a.sekPerKWh()));
+
+        System.out.println("list after sorting: " + sortedCopy);
     }
+
+
+
+    // Function for Determining optimal charging windows:
+
+    public static void optimalWindow(List<ElpriserAPI.Elpris> prices, int duration) {
+        if(duration == 2 || duration == 4 || duration == 8){
+            // continue
+        }
+        else {
+            System.out.println("The duration must be 2, 4, or 8 hour window!");
+            return;
+        }
+
+        double minPriceSum = Double.MAX_VALUE;
+        int starting = 0;
+        double sum = 0;
+
+        for (int i = 0; i < duration; i++) {
+            sum += prices.get(i).sekPerKWh();
+        }
+        minPriceSum = sum;
+
+        for (int i = duration; i < prices.size(); i++) {
+            sum = sum - prices.get(i - duration).sekPerKWh() + prices.get(i).sekPerKWh();
+
+            if (sum < minPriceSum) {
+                minPriceSum = sum;
+                starting = i - duration + 1;
+            }
+        }
+
+        int ending = starting + duration - 1;
+
+        int[] optimalArr = {starting, ending};
+
+
+        System.out.println("optimal hours:");
+        for (int i = optimalArr[0]; i <= optimalArr[1]; i++) {
+            ElpriserAPI.Elpris p = prices.get(i);
+            System.out.println(p.timeStart().getHour());
+        }
+
+    }
+
+
 
 
 
